ce05f578a755287a83a2a6d3b7ac7e72513a62e5
RaveeshSehrawat <sehrawatraveesh88@gmail.com>
2025-08-07 21:36:41 +0530

    updated vite config js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Portfolio-Frontend/",
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
